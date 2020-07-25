85590380b38b8c7b1136aaca30cb0d8407208f64
bonasunu <bonaventurasunu@gmail.com>
2020-07-25 22:44:24 +0700

    add script for rock paper scissors algorithms

--- main.js
+++ main.js
@@ -9,3 +9,35 @@
 // display result using DOM
 
 // implement refresh button - display none to result info
+
+
+const winTable = [
+    ['d', 'p', 'c'],
+    ['c', 'd', 'p'],
+    ['p', 'c', 'd']
+]
+
+const result = player => {
+    if (player === 'd') {
+        console.log('Draw!')
+    } else if (player === 'p') {
+        console.log('Player 1 win')
+    } else if (player === 'c') {
+        console.log('Com win')
+    }
+}
+
+const choose = choice => {
+    const playerChoice = ['rock', 'paper', 'scissors']
+    console.log('Player 1 choose', playerChoice[choice])
+
+    const random = Math.floor(Math.random() * 3)
+    
+    const gameResult = winTable[random][choice]
+
+    result(gameResult)
+}
+
+const refresh = () => {
+    console.log('Refresh!')
+}
