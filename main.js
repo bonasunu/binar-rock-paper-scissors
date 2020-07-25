320e183b46bb76303adb92a6f665f3230202b3ad
bonasunu <bonaventurasunu@gmail.com>
2020-07-25 20:22:50 +0700

    implement figma design

---
+++ main.js
@@ -0,0 +1,11 @@
+// make function for onclick html element
+
+// console log choice
+
+// randomiza com choice
+
+// pass the result to result function, log the result
+
+// display result using DOM
+
+// implement refresh button - display none to result info
