22bad0adcbc69f80f7035bba0bfa65021bf2cb78
bonasunu <bonaventurasunu@gmail.com>
2020-07-29 19:38:39 +0700

    fix indentation on all files

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // two dimentional array for game result probability
 // 'd' = draw, 'p' = player 1 win, 'c' = com win
 const WIN_TABLE = [
-    ['d', 'p', 'c'],
-    ['c', 'd', 'p'],
-    ['p', 'c', 'd']
+  ['d', 'p', 'c'],
+  ['c', 'd', 'p'],
+  ['p', 'c', 'd']
 ]
 
 // disable player 1 input after player 1 selected an option
 const disableInput = () => {
-    document.querySelectorAll('.choice-player').forEach(input => {
-        input.setAttribute('disabled', 'disabled')
-    })
+  document.querySelectorAll('.choice-player').forEach(input => {
+    input.setAttribute('disabled', 'disabled')
+  })
 }
 
 // hide versus info when result is visible
@@ -32,43 +32,42 @@ const showResult = result => {
 
 // get player 1 choice and compare it to com choice. Then invoke show game result function
 const getChoice = choice => {
-    const PLAYER_CHOICE = ['rock', 'paper', 'scissors']
-    const COM_CHOICE = ['com-rock', 'com-paper', 'com-scissors']
+  const PLAYER_CHOICE = ['rock', 'paper', 'scissors']
+  const COM_CHOICE = ['com-rock', 'com-paper', 'com-scissors']
 
-    console.log('Player 1 choose', PLAYER_CHOICE[choice])
-    const random = Math.floor(Math.random() * 3) // randomize com choice
+  console.log('Player 1 choose', PLAYER_CHOICE[choice])
+  const random = Math.floor(Math.random() * 3) // randomize com choice
 
-    document.getElementById(PLAYER_CHOICE[choice]).classList.add('chosen')
-    document.getElementById(COM_CHOICE[random]).classList.add('chosen')
-    document.querySelectorAll('.choice-player').forEach(choice => {
-        choice.classList.remove('choice-hover');
-        choice.style.cursor = 'default';
-    })
-    
-    const gameResult = WIN_TABLE[random][choice] // get the result by accessing the result probability in WIN_TABLE
-
-    showResult(gameResult) // invoke showResult to show the result in browser
+  document.getElementById(PLAYER_CHOICE[choice]).classList.add('chosen')
+  document.getElementById(COM_CHOICE[random]).classList.add('chosen')
+  document.querySelectorAll('.choice-player').forEach(choice => {
+    choice.classList.remove('choice-hover');
+    choice.style.cursor = 'default';
+  })
+  
+  const gameResult = WIN_TABLE[random][choice] // get the result by accessing the result probability in WIN_TABLE
+  showResult(gameResult) // invoke showResult to show the result in browser
 }
 
 // implement refresh game to restart the game
 const refreshGame = () => {
-    console.log('Refresh!')
+  console.log('Refresh!')
 
-    document.querySelectorAll('.choice-player').forEach(input => {
-        input.removeAttribute('disabled')
-    })
+  document.querySelectorAll('.choice-player').forEach(input => {
+    input.removeAttribute('disabled')
+  })
 
-    document.getElementById('vs').style.display = 'block'
-    document.getElementById('draw').style.display = 'none';
-    document.getElementById('win-player').style.display = 'none';
-    document.getElementById('win-com').style.display = 'none';
+  document.getElementById('vs').style.display = 'block'
+  document.getElementById('draw').style.display = 'none';
+  document.getElementById('win-player').style.display = 'none';
+  document.getElementById('win-com').style.display = 'none';
 
-    document.querySelectorAll('.choice-player').forEach(choice => {
-        choice.classList.add('choice-hover');
-        choice.style.cursor = 'pointer';
-    })
+  document.querySelectorAll('.choice-player').forEach(choice => {
+    choice.classList.add('choice-hover');
+    choice.style.cursor = 'pointer';
+  })
 
-    document.querySelectorAll('.figure').forEach(figure => {
-        figure.classList.remove('chosen');
-    })
+  document.querySelectorAll('.figure').forEach(figure => {
+    figure.classList.remove('chosen');
+  })
 }
